8ca925206ae00010aeba934dafd039458298edf9
JPMorgan22 <jpmorganjeremy@yahoo.com>
2022-07-17 02:33:45 +0000

    added user registration functionality

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -43,7 +43,33 @@ def login_request(request):
 # Create a `registration_request` view to handle sign up request
 def registration_request(request):
     context = {}
-    if request.method == "GET":
+
+    if request.method == "POST":
+        # Get user information from request.POST
+        username = request.POST['username']
+        password = request.POST['password']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            # Check if user already exists
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            # If not, simply log this is a new user
+            logger.debug("{} is new user".format(username))
+        # If it is a new user
+        if not user_exist:
+            # Create user in auth_user table
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            # Login the user and redirect to course list page
+            login(request, user)
+            return redirect("djangoapp:about")
+        else:
+            return render(request, 'djangoapp/signup.html', context)
+
+    elif request.method == "GET":
         return render(request, 'djangoapp/signup.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
